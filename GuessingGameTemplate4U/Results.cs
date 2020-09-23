61db5b105145cb7f4289f0f81a23d822b1c2e2b3
Victoria Jackson <snowstorm12345678@gmail.com>
2020-09-23 10:13:56 -0400

    Just adding my name

--- GuessingGameTemplate4U/Results.cs
+++ GuessingGameTemplate4U/Results.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GuessingGameTemplate4U
+    /* 
+     * Jayden Roddick 
+     * Guessing Game 
+     * 09/23/20
+     */
 {
     public partial class Results : UserControl
     {
@@ -20,19 +25,15 @@ namespace GuessingGameTemplate4U
         private void Results_Load(object sender, EventArgs e)
         {
             for (int i = 0; i < Form1.guesses.Count(); i++)
-
             {
-
                results2lable.Text +=  Form1.guesses[i] + " ";
-
             }
+
             Form1.guesses.Sort();
+
             for (int i = 0; i < Form1.guesses.Count(); i++)
-
             {
-
                 label3results.Text += Form1.guesses[i] + " ";
-
             }
         }
     }
